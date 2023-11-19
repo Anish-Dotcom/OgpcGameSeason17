2a3ed2a544c88a8dfccfdf9828b9e810200a018d
i1aw <101474663+i1aw@users.noreply.github.com>
2023-11-19 09:11:22 -0800

    Added Main Menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public float width = 225;
+    public float height = 40;
+    public float hoverWidth = 250;
+    public float animationSpeed = 3;
+    public int sceneNumber = 0;
+    private bool pointerHovering;
+    public void openScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void pointerEnter() // used with the event trigger component
+    {
+        pointerHovering = true;
+    }
+    public void pointerExit() // used with the event trigger component
+    {
+        pointerHovering = false;
+    }
+
+    void FixedUpdate()
+    {
+        // find the difference between the width of the button and the target width (depends on if the pointer is hovering over the button)
+        RectTransform rt = gameObject.GetComponent<RectTransform>();
+        float difference = (pointerHovering ? hoverWidth : width) - rt.sizeDelta.x;
+        // resize the button by a faction of the difference. I used fixed update to make this animation run the same speed at all frame rates.
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x + (difference / animationSpeed), height);
+    }
+
+}
