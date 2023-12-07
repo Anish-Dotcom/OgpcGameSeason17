b5b164ead46eacb74068fb431036f09265a82a7c
Zypheruss <470103@bsd48.org>
2023-12-07 06:43:25 -0800

    Animation added for tall fire
    The short fires are disabled right now because I havent animated them yet. Just wanted to save my changes

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -14,17 +14,30 @@ public class FireController : MonoBehaviour
     public float lightIntensity;
     public GameObject fireLight;
 
+    public Sprite[] fireObjForAni1;
+    public Sprite[,] fireObjectsForAnimation;//Make 2d array with all fire objects
+    public int aniNum = 0;
+
     void Start()
     {
         //randSeed = Random.Range(-1.0f, 1.0f);
         if (objType == "fireCon") 
         {
+            fireObjectsForAnimation = new Sprite[fireObjForAni1.Length / (fireObjForAni1.Length / 4), fireObjForAni1.Length / 4];
+            for (int i = 0; i < fireObjForAni1.Length / 4; i++)//does for number of frames
+            {
+                for (int j = 0; j < fireObjForAni1.Length / (fireObjForAni1.Length / 4); j++)//Does for each frame
+                {
+                    fireObjectsForAnimation[j, i] = fireObjForAni1[j + (i * 4)];
+                }
+            }
             fireObjects = new GameObject[transform.childCount];
             for (int i = 0; i < transform.childCount; i++)
             {
                 fireObjects[i] = transform.GetChild(i).gameObject;
             }
             InvokeRepeating("FireMovement", 0f, 0.08f);
+            InvokeRepeating("AnimateFire", 0f, 0.5f);
         }
         else if (objType == "lightCon") 
         {
@@ -41,14 +54,38 @@ public class FireController : MonoBehaviour
         for (int i = 0; i < fireObjects.Length; i++)
         {
             //Debug.Log(totalTime);
-            fireHeight = 0.7f + (Mathf.Sin((randSeed) + 2 * totalTime) / (Random.Range(6.0f, 6.2f) + i / 4));
-            fireWidth = 0.7f + (Mathf.Cos((randSeed) + 2 * totalTime) / (Random.Range(8.0f, 8.2f) + i / 4));
+            fireHeight = 0.7f + (Mathf.Sin((1.3f * totalTime) + 2 * randSeed) / (Random.Range(6.0f, 6.2f) + i / 4));
+            fireWidth = 0.7f + (Mathf.Cos((1.3f * totalTime) + 2 * randSeed) / (Random.Range(8.0f, 8.2f) + i / 4));
             //Debug.Log(fireHeight + fireWidth);
             fireObjects[i].transform.localScale = new Vector3(fireWidth, fireHeight, 1);
         }
     }
     public void LightChanges() 
     { 
-        fireLight.GetComponent<Light>().intensity = 3.5f + 6 * (Mathf.Sin((randSeed) + 2 * totalTime) / (Random.Range(6.0f, 6.2f)));
+        fireLight.GetComponent<Light>().intensity = 3.5f + 6 * (Mathf.Sin((1.3f * totalTime) + 2 * randSeed) / (Random.Range(6.0f, 6.2f)));
+    }
+    public void AnimateFire() 
+    {
+        for (int i = 0; i < fireObjects.Length; i++)
+        {
+            fireObjects[i].transform.GetComponent<UnityEngine.UI.Image>().sprite = fireObjectsForAnimation[i, aniNum];
+            if (aniNum == 1) 
+            {
+                Debug.Log(fireObjects[i].transform.eulerAngles);
+                Vector3 fireObj = fireObjects[i].transform.eulerAngles;
+                fireObj.x *= -1f;
+                fireObj.y += 180f;
+                fireObj.z *= -1f;
+                fireObjects[i].transform.eulerAngles = fireObj;
+            }
+        }
+        if (aniNum == 2)
+        {
+            aniNum = 0;
+        }
+        else
+        {
+            aniNum++;
+        }
     }
 }
