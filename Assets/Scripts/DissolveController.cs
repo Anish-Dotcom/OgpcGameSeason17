0e909dc2e36db13bc73fb0c1809cc84b51d752b8
Zypheruss <470103@bsd48.org>
2023-12-31 12:35:26 -0800

    New materials and methods
    Made the dissolve shader from scratch, the toon shader I made was clunky, ended up deciding to use free unity store one

---
+++ Assets/Scripts/DissolveController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    public float dissolveDistance;//The distance in radius around the centralPos that things will apear
+    public GameObject centralObj;//able to change positon to cleanly make something appear
+    public Material areaMat;
+
+    void Start()
+    {
+        areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
+        areaMat.SetFloat("_Cutoff_Distance", dissolveDistance);
+    }
+    public void SetObjPos()
+    {
+        areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
+    }
+    public void SetCutoffDist() 
+    {
+        areaMat.SetFloat("_Cutoff_Distance", dissolveDistance);
+    }
+}
