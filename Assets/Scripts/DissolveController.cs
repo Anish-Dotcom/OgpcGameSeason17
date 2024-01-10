1a7578a556af494e974eb0bbe38d21de10234ac8
bradyRozendaal <144848905+bradyRozendaal@users.noreply.github.com>
2024-01-10 14:09:27 -0800

    Added materials and colliders to new objects
    Made it look*

--- Assets/Scripts/DissolveController.cs
+++ Assets/Scripts/DissolveController.cs
@@ -13,10 +13,6 @@ public class DissolveController : MonoBehaviour
         areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
         areaMat.SetFloat("_Cutoff_Distance", dissolveDistance);
     }
-    void Update() 
-    {
-        areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
-    }
     public void SetObjPos()
     {
         areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
