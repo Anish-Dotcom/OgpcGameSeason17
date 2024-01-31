b35ea8fa720cdca18df4f7f94ad45312c6ccdfc8
Zypheruss <470103@bsd48.org>
2024-01-30 17:35:32 -0800

    all directions for dissolve effect
    able to set distances for x, y, & z by themselves so you can have different sized rooms. (like our main room)

--- Assets/Scripts/Visuals/DissolveController.cs
+++ Assets/Scripts/Visuals/DissolveController.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class DissolveController : MonoBehaviour
 {
-    public float dissolveDistance;//The distance in radius around the centralPos that things will apear
+    public Vector3 dissolveDistances;//The distance in radius around the centralPos that things will apear
     public GameObject centralObj;//able to change positon to cleanly make something appear
     public Material areaMat;
 
     void Start()
     {
         areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
-        areaMat.SetFloat("_Cutoff_Distance", dissolveDistance);
+        areaMat.SetFloat("_Cutoff_Distance_X", dissolveDistances.x);
+        areaMat.SetFloat("_Cutoff_Distance_Y", dissolveDistances.y);
+        areaMat.SetFloat("_Cutoff_Distance_Z", dissolveDistances.z);
     }
     public void SetObjPos()
     {
@@ -19,6 +21,8 @@ public class DissolveController : MonoBehaviour
     }
     public void SetCutoffDist() 
     {
-        areaMat.SetFloat("_Cutoff_Distance", dissolveDistance);
+        areaMat.SetFloat("_Cutoff_Distance_X", dissolveDistances.x);
+        areaMat.SetFloat("_Cutoff_Distance_Y", dissolveDistances.y);
+        areaMat.SetFloat("_Cutoff_Distance_Z", dissolveDistances.z);
     }
 }
