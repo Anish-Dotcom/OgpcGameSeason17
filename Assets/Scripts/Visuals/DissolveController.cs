ad461409a7b296427e8527a8cba07735a976aec9
bradyRozendaal <144848905+bradyRozendaal@users.noreply.github.com>
2024-02-01 13:57:12 -0800

    Dissolve fixed (fr this time)

--- Assets/Scripts/Visuals/DissolveController.cs
+++ Assets/Scripts/Visuals/DissolveController.cs
@@ -8,9 +8,17 @@ public class DissolveController : MonoBehaviour
     public GameObject centralObj;//able to change positon to cleanly make something appear
     public Material areaMat;
 
+    void Update()
+    {
+        areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
+        areaMat.SetFloat("_Cutoff_Distance_X", dissolveDistances.x);
+        areaMat.SetFloat("_Cutoff_Distance_Y", dissolveDistances.y);
+        areaMat.SetFloat("_Cutoff_Distance_Z", dissolveDistances.z);
+    }
     void Start()
     {
         areaMat.SetVector("_Object_Position_For_Ref_Dis", centralObj.transform.position);
+        Debug.Log(areaMat.GetVector("_Object_Position_For_Ref_Dis"));
         areaMat.SetFloat("_Cutoff_Distance_X", dissolveDistances.x);
         areaMat.SetFloat("_Cutoff_Distance_Y", dissolveDistances.y);
         areaMat.SetFloat("_Cutoff_Distance_Z", dissolveDistances.z);
