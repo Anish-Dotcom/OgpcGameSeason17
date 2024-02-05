727093336b1c749ef4e48e6a840104076a595953
bradyRozendaal <144848905+bradyRozendaal@users.noreply.github.com>
2024-02-05 09:13:45 -0800

    Box burning system not done

--- Assets/Scripts/Visuals/BurnController.cs
+++ Assets/Scripts/Visuals/BurnController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BurnController : MonoBehaviour
 {
-    public GameObject firePrefab;
+    public GameObject[] firePrefabs;
+    public GameObject fireCanvas;
 
 
     // Start is called before the first frame update
@@ -21,9 +22,28 @@ public class BurnController : MonoBehaviour
 
     void OnTriggerEnter(Collider col)//burn object
     {
-        if (col.gameObject.CompareTag("Box"))
+        if (col.gameObject.CompareTag("Box"))//and hasnt been burned
         {
-            Instantiate(firePrefab, new Vector3(0, 0, 0), Quaternion.identity, col.gameObject.GetComponent<Transform>());
+            int fireCanvasIndex = 0;
+
+            int NumberOfFlames = Random.Range(1, 6);
+            for (int i = 0; i < NumberOfFlames; i++)
+            {
+                if (i == 0)//instatiate canvas
+                {
+                    Instantiate(fireCanvas, new Vector3(0, 0, 0), Quaternion.identity, col.gameObject.GetComponent<Transform>());
+                    for (int b = 0; b < col.gameObject.GetComponent<Transform>().childCount; b++)
+                    {
+                        if (col.gameObject.GetComponent<Transform>().GetChild(b).name == "Fire Canvas")
+                        {
+                            fireCanvasIndex = b;
+                            break;
+                        }
+                    }
+                }
+                int typeOfFlame = Random.Range(0, 2);//either 0, or 1 (0 is short, 1 is tall)
+                Instantiate(firePrefabs[typeOfFlame], new Vector3(0, 0, 0), Quaternion.identity, col.gameObject.GetComponent<Transform>().GetChild(fireCanvasIndex).GetChild(0));
+            }
             //make it look like its burning and then turn to ash, add particles to the fire.
         }
     }
