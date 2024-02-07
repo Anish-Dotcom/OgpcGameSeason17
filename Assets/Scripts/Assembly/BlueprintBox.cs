4471bd19cdc924a9f44ef4b67e798b54159de585
i1aw <101474663+i1aw@users.noreply.github.com>
2024-02-07 10:19:26 -0800

    Updated blueprint box to use menu controller
    I also fixed the interact pop up

--- Assets/Scripts/Assembly/BlueprintBox.cs
+++ Assets/Scripts/Assembly/BlueprintBox.cs
@@ -7,63 +7,44 @@ public class BlueprintBox : MonoBehaviour
     public GameObject interact;
     public GameObject blueprintMenu;
     public bool blueprintIsOpen = false;
+    public bool lookingAtBox = false;
     public float distFromObject = 0;
     public GameObject playerCam;
+    public MenuController menuController;
 
-    public float moveSpeedSaved;
-    public float sensXSaved;
-    public float sensYSaved;
-
-
-    void Start()
-    {
-
-    }
     void Update()
     {
+        if (lookingAtBox)
+        {
+            menuController.openPopup(interact);
+        } else
+        {
+            menuController.closePopup(interact);
+        }
+    }
+    private void OnMouseOver()
+    {
+        lookingAtBox = true;
+        distFromObject = Vector3.Distance(playerCam.transform.position, transform.position);
         if (Input.GetKeyDown(KeyCode.E))
         {
             distFromObject = Vector3.Distance(playerCam.transform.position, transform.position);
 
             if (distFromObject <= 3.5 && !blueprintIsOpen)
             {
-                blueprintMenu.SetActive(true);
+                menuController.openMenu(blueprintMenu);
                 blueprintIsOpen = true;
-                moveSpeedSaved = PlayerMove.moveSpeed;//used so when you exit menu you can set back to previous, prob better to just have this saved somewhere else and you can only change current move speed, but still have saved move speed, same with sense x & y
-                PlayerMove.moveSpeed = 0f;
-                sensXSaved = PlayerCam.sensX;
-                sensYSaved = PlayerCam.sensY;
-                PlayerCam.sensX = 0f;
-                PlayerCam.sensY = 0f;
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (blueprintIsOpen)
+            {
+                menuController.closeMenu(blueprintMenu);
+                blueprintIsOpen = false;
             }
         }
     }
-    private void OnMouseOver()
-    {
-        distFromObject = Vector3.Distance(playerCam.transform.position, transform.position);
-        //Debug.Log(distFromObject);
 
-        if (distFromObject <= 3.5 && !blueprintIsOpen)
-        {
-            Debug.Log("open blueprint?");
-            interact.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("set active false");
-            interact.SetActive(false);
-        }
-    }
-    public void CloseBlueprintMenu()
+    public void OnMouseExit()
     {
-        blueprintIsOpen = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        PlayerMove.moveSpeed = moveSpeedSaved;
-        PlayerCam.sensX = sensXSaved;
-        PlayerCam.sensY = sensYSaved;
-        blueprintMenu.SetActive(false);
+        lookingAtBox = false;
     }
 }
