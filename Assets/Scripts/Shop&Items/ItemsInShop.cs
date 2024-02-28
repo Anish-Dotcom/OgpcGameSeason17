7b7d8fed3b6f4c112f402e2d3ab7876db4ef1cf1
ayanmas <460146@bsd48.org>
2024-02-27 18:00:58 -0800

    shop system continued. unfinished

--- Assets/Scripts/Shop&Items/ItemsInShop.cs
+++ Assets/Scripts/Shop&Items/ItemsInShop.cs
@@ -2,66 +2,153 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-/*
+
 [System.Serializable]
 public class ItemProperties
 {
-    public bool isForWindupToy;
+    // which tab will it fall under:
+    public bool sales = false;
+    public bool combos = false;
+    public bool innerParts = false;
+    public bool outterParts = false;
+    public bool decoratives = false;
+
+    public bool isForWindupToy = false;
     public string itemName;
     public GameObject itemObject;
     public float itemPrice;
     public Sprite itemImage;
     public string[] daysSold; // days of the week
+    public int currentItemQuantity; // quantity before buying
 }
 
 public class ItemsInShop : MonoBehaviour
 {
+    public float playerMoney;
     public GameObject prefabButtonInShop; // viewable button you see when you enter shop
-    private GameObject[] prefabButtons; // editted prefab of standard
-    public Button[] itemButtons;
+    private List<GameObject> prefabButtons = new List<GameObject>(); // editted prefab of standard
+    public Transform itemsToGoObject; // where the editted prefab gets instantiated
+
+    public GameObject prefabBuying;
+    public Transform prefabToGoBuyingObject;
+
+    public Text playerMoneyText;
+
     public List<ItemProperties> properties = new List<ItemProperties>(); // list of all the item in shops properties
 
     void Start()
     {
-        for(int i = 0; i < properties.Count; i++)
+        CreateShopButtons();
+        UpdatePlayerMoney();
+    }
+
+    void CreateShopButtons()
+    {
+        for (int i = 0; i < properties.Count; i++) // assigns the name and price to be viewed
         {
-            prefabButtons[i] = Instantiate(prefabButtonInShop, transform);
-            if(prefabButtons[i].GetComponentInChildren<GameObject>().name == "itemname")
-            {
-                prefabButtons[i].GetComponentInChildren<Text>().text = properties[i].itemName;
-            }
-            if (prefabButtons[i].GetComponentInChildren<GameObject>().name == "itemprice")
-            {
-                prefabButtons[i].GetComponentInChildren<Text>().text = properties[i].itemPrice.ToString();
-            }
-            itemButtons[i] = prefabButtons[i].GetComponentInChildren<Button>();
-            itemButtons[i].onClick.AddListener(() => OnButtonClick(itemButtons[i].gameObject));
+            GameObject button = Instantiate(prefabButtonInShop, itemsToGoObject);
+            prefabButtons.Add(button);
+
+            Transform buttonTransform = button.transform;
+            Image itemImage = buttonTransform.GetChild(0).GetComponent<Image>();
+            Text itemNameText = buttonTransform.GetChild(1).GetComponent<Text>();
+            Text itemPriceText = buttonTransform.GetChild(2).GetComponent<Text>();
+            Button buyButton = buttonTransform.GetChild(3).GetComponent<Button>();
+
+            //itemImage.sprite = properties[i].itemImage;
+            itemNameText.text = properties[i].itemName;
+            itemPriceText.text = properties[i].itemPrice.ToString();
+
+            int index = i; // Capturing the correct index for whats below
+            buyButton.onClick.AddListener(() => BuyItem(index));
         }
     }
 
-    void OnButtonClick(GameObject buttonClicked)
+    void BuyItem(int index) 
     {
-        foreach (GameObject obj in prefabButtons)
+        ItemProperties item = properties[index];
+        if (playerMoney >= item.itemPrice)
         {
-            if (obj == buttonClicked.transform.parent.gameObject)
-            {
-                quantity--;
-                ItemManager.totalAmountObj--;
-                reformatThePrice();
-                UpdateUI(obj);
-                UpdateArray();
+            playerMoney -= item.itemPrice;
+            UpdatePlayerMoney();
 
-                if (quantity <= 0)
-                {
-                    instantiatedObjects.Remove(obj);
-                    Destroy(obj);
-                }
-
-                break;
-            }
+            item.currentItemQuantity++;
+            UpdateBuyingUI(item);
         }
+        else
+        {
+            Debug.Log("Not enough money to buy this item!");
+        }
+    }
+
+    void UpdateBuyingUI(ItemProperties item)
+    {
+        GameObject buyingButton = Instantiate(prefabBuying, prefabToGoBuyingObject);
+        Transform buyingButtonTransform = buyingButton.transform;
+        Text itemNameText = buyingButtonTransform.GetChild(0).GetComponent<Text>();
+        Text itemPriceText = buyingButtonTransform.GetChild(1).GetComponent<Text>();
+        Button noBuyButton = buyingButtonTransform.GetChild(3).GetComponent<Button>();
+
+        itemNameText.text = "- " + item.itemName + " (" + item.currentItemQuantity + ")";
+        itemPriceText.text = (item.itemPrice * item.currentItemQuantity).ToString();
+        noBuyButton.onClick.AddListener(() => RemoveItem(item));
+    }
+
+    void RemoveItem(ItemProperties item)
+    {
+        item.currentItemQuantity--;
+        UpdateBuyingUI(item);
+        if (item.currentItemQuantity == 0)
+        {
+            ResetShop();
+        }
+        else
+        {
+            foreach (GameObject button in prefabButtons)
+            {
+                Destroy(button);
+            }
+            prefabButtons.Clear();
+            CreateShopButtons();
+        }
+    }
+
+    public void Purchase()
+    {
+        float totalPrice = 0;
+        foreach (Transform child in prefabToGoBuyingObject)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (ItemProperties item in properties)
+        {
+            totalPrice += item.itemPrice * item.currentItemQuantity;
+            item.currentItemQuantity = 0;
+        }
+
+        playerMoney -= totalPrice;
+        UpdatePlayerMoney();
+    }
+
+    public void UpdatePlayerMoney()
+    {
+        playerMoneyText.text = "$" + playerMoney.ToString();
+    }
+
+    public void ResetShop()
+    {
+        foreach (GameObject button in prefabButtons)
+        {
+            Destroy(button);
+        }
+        prefabButtons.Clear();
+        CreateShopButtons();
     }
 }
+
+/* this is the old shop script which is so ancient I no longer understand why I did some of the things I did in it so I'm remade it.
+
 
 public class ItemsInShop : MonoBehaviour
 {
