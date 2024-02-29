6427473c69ae03256e45b93d66ad58641866bfc3
bradyRozendaal <144848905+bradyRozendaal@users.noreply.github.com>
2024-02-29 14:27:08 -0800

    Working on commission controller

--- Assets/Scripts/Comission/ComissionController.cs
+++ Assets/Scripts/Comission/ComissionController.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComissionController : MonoBehaviour
 {
-    public string[] variableNames;//names of each of the perameters
-
     public string[] names;//names of people for comission text
     public string[] toyTypes;//types of toys that can be requested 
     public string[] colors;//Color you want for the toy
+    public Vector3[] colorRGBValues;
     public string[] flavorText;
 
     public string requestText;
+    public int numberOfComissions;
+    public Text currectCommission;
+
 
     void Start()
     {
-        //SetNewComission();
+        numberOfComissions = 0;
+        SetNewComission();
     }
 
     // Update is called once per frame
@@ -26,10 +30,26 @@ public class ComissionController : MonoBehaviour
 
     public void SetNewComission()
     {
-        int[] inputNums = new int[variableNames.Length];//random name of person, type of toy, and color, can add more perameters
-        for (int i = 0; i < variableNames.Length; i++)
+        numberOfComissions++;
+        int[] inputNums = new int[4];//random name of person, type of toy, and color, can add more perameters
+        for (int i = 0; i < 4; i++)
         {
-            inputNums[i] = Random.Range(0, variableNames[i].Length);//this does the length of the word, not the array
+            if (i == 0)
+            {
+                inputNums[i] = Random.Range(0, names.Length);
+            }
+            else if (i == 1)
+            {
+                inputNums[i] = Random.Range(0, toyTypes.Length);
+            }
+            else if (i == 2)
+            {
+                inputNums[i] = Random.Range(0, colors.Length);
+            }
+            else if (i == 3)
+            {
+                inputNums[i] = Random.Range(0, flavorText.Length);
+            }
             Debug.Log(inputNums[i]);
         }
 
@@ -37,7 +57,7 @@ public class ComissionController : MonoBehaviour
         sentenceStructureRand = 0;//for testing
         if (sentenceStructureRand == 0)
         {
-            requestText = "Hey i'm " + names[inputNums[0]] + " " + flavorText[inputNums[3]] + ". I want a " + toyTypes[inputNums[1]] + " in the color " + colors[inputNums[inputNums[2]]];
+            requestText = "Hey I'm " + names[inputNums[0]] + ". " + flavorText[inputNums[3]] + " I want a " + toyTypes[inputNums[1]] + " in the color " + colors[inputNums[2]] + ".";
             Debug.Log(requestText);
         }
         else if (sentenceStructureRand == 1)
@@ -56,10 +76,11 @@ public class ComissionController : MonoBehaviour
         {
             //requestText = ;
         }
+        currectCommission.text = names[inputNums[0]] + " requested a " + colors[inputNums[2]] + ", " + toyTypes[inputNums[1]] + ".";
     }
 
     public void RemoveOldComission()
     {
-
+        numberOfComissions--;
     }
 }
