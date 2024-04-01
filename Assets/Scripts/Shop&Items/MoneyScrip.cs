5249073b8d0c81b7f8e843bd9d3550fdc5ac893c
ayan masud <460146@bsd48.org>
2024-04-01 14:27:14 -0700

    money system + doll prefab fix

--- Assets/Scripts/Shop&Items/MoneyScrip.cs
+++ Assets/Scripts/Shop&Items/MoneyScrip.cs
@@ -8,12 +8,15 @@ public class MoneyScrip : MonoBehaviour
     public float currentMoney;
     public float unChangedMoney;
     public Text moneyText;
+    public Text increaseText;
+    public CanvasGroup increaseTextCanvasGroup;
+    public Text decreaseText;
+    public CanvasGroup decreaseTextCanvasGroup;
     public float amount2;
     // Start is called before the first frame update
     void Start()
     {
 
-        DecreaseMoney(10);
     }
 
     // Update is called once per frame
@@ -29,11 +32,25 @@ public class MoneyScrip : MonoBehaviour
     {
         unChangedMoney = currentMoney;
         amount2 = amount;
+        increaseText.text = "+" + amount.ToString("F2");
 
         InvokeRepeating("TextRollUp", 0, 0.0001f);
 
+        increaseTextCanvasGroup.alpha = 1;
+        InvokeRepeating("increaseDisappear", 1.5f, 0.001f);
 
     }
+
+    public void increaseDisappear()
+    {
+        increaseTextCanvasGroup.alpha -= 0.1f;
+        if (increaseTextCanvasGroup.alpha == 0)
+        {
+            increaseTextCanvasGroup.alpha = 0;
+            CancelInvoke("increaseDisappear");
+        }
+    }
+
     public void TextRollUp()
     {
         currentMoney += amount2 / 200;
@@ -47,11 +64,24 @@ public class MoneyScrip : MonoBehaviour
     {
         unChangedMoney = currentMoney;
         amount2 = amount;
+        decreaseText.text = "-" + amount.ToString("F2");
 
         InvokeRepeating("TextRollDown", 0, 0.0001f);
 
-
+        decreaseTextCanvasGroup.alpha = 1;
+        InvokeRepeating("decreaseDisappear", 1.5f, 0.001f);
     }
+
+    public void decreaseDisappear()
+    {
+        decreaseTextCanvasGroup.alpha -= 0.1f;
+        if (decreaseTextCanvasGroup.alpha == 0)
+        {
+            decreaseTextCanvasGroup.alpha = 0;
+            CancelInvoke("decreaseDisappear");
+        }
+    }
+
     public void TextRollDown()
     {
         currentMoney -= amount2 / 200;
